ba585618312d17b36730ab8eafdd72a0faee0eeb
unknown <nwanetifelix@gmail.com>
2024-09-11 02:55:39 +0100

    Made the Nav Bar Functional

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,11 @@
+import { Link } from "react-router-dom";
 import styles from "./nav.module.css";
 export default function Nav() {
-  return <div className={styles.nav}>🍔 FoodApp</div>;
+  return (
+    <div className={styles.nav}>
+      <Link className={styles.navlink} to="/">
+        🍔 FoodApp
+      </Link>
+    </div>
+  );
 }
